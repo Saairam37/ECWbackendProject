9155a51687426dc6fe85c6e2a9d9bcd731d78dee
Saairam37 <saairam185@gmail.com>
2025-08-26 04:53:13 +0530

    no thing

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ dotenv.config({path:path.join(__dirname,"config/config.env")});
 
 
 app.use(cors({
-  origin: 'http://localhost:5173', // Or list multiple allowed origins
-  credentials: true,              // If you need cookies/auth headers
+  origin: 'http://localhost:5173',
+  credentials: true,              
 }));
 app.options('*', cors());
 
@@ -28,12 +28,6 @@ app.use('/api/v1/',auth);
 app.use('/api/v1/',order);
 app.use('/api/v1/',payment);
 
-if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, '../frontend/build')));
-    app.get(/.*/, (req, res) =>{
-        res.sendFile(path.resolve(__dirname, '../build/index.html'))
-    })
-}
 
 app.use(errorMiddleware)
 
