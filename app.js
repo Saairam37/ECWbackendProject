b35410ba8065ad96274628d092719899e6666123
Saairam37 <saairam185@gmail.com>
2025-08-26 04:29:19 +0530

    cors

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ const express = require('express');
 const app = express();
 const errorMiddleware = require('./middlewares/error');
 const cookieParser = require('cookie-parser')
-const path = require('path')
+const path = require('path');
+const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config({path:path.join(__dirname,"config/config.env")});
 
+
+app.use(cors({
+  origin: 'http://localhost:5173', // Or list multiple allowed origins
+  credentials: true,              // If you need cookies/auth headers
+}));
+app.options('*', cors());
 
 app.use(express.json());
 app.use(cookieParser());
